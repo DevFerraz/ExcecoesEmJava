a79c731f8058ebb9627db588b0cd7cde558b9d0d
DevFerraz <104537447+DevFerraz@users.noreply.github.com>
2022-11-04 12:47:52 -0300

    Exceções em Java

---
+++ src/TesteConexao.java
@@ -0,0 +1,19 @@
+public class TesteConexao {
+    public static void main(String[] args) {
+        try (Conexao con = new Conexao()) {
+            con.leDados();
+        } catch (IllegalStateException exception) {
+            System.out.println("Deu erro na conexao");
+        }
+    }
+}
+/*Conexao con = null;
+        try {
+            con = new Conexao();
+            con.leDados();
+        }catch (IllegalStateException exception){
+            System.out.println("Deu erro na conexao");
+        }finally {
+            con.fecha();
+        }
+    }*/
